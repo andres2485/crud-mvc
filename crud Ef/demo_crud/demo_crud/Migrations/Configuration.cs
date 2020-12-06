8698bb087070bcad0d411cf10d10aa7713e68950
andres2485 <claudio.quezada@virginiogomez.cl>
2020-12-05 21:01:45 -0300

    primer commit

---
+++ crud Ef/demo_crud/demo_crud/Migrations/Configuration.cs
@@ -0,0 +1,24 @@
+﻿namespace demo_crud.Migrations
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<demo_crud.Models.UsuarioContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = false;
+            ContextKey = "demo_crud.Models.UsuarioContext";
+        }
+
+        protected override void Seed(demo_crud.Models.UsuarioContext context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method
+            //  to avoid creating duplicate seed data.
+        }
+    }
+}
